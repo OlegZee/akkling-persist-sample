e324b8cacf1b93f0e1d5214618fc988cdac1d705
Oleg Zaimkin <olegzzzz@gmail.com>
2018-05-20 15:04:33 +0700

    this works with JObject intercepted hack (and some more code)

--- Program.fs
+++ Program.fs
@@ -1,6 +1,42 @@
-﻿open Akkling
+﻿open System
+open Akkling
 open Akkling.Persistence
+open Newtonsoft.Json.Linq
 
+let eventType = "eventType"
+let anyEventTag = "anyEventTag"
+
+let private deserializeEvent (evt : obj) =
+    let json = evt :?> Newtonsoft.Json.Linq.JObject
+    let objectType = json.[eventType]
+    let typ = if objectType = null then null else Type.GetType(objectType.ToString())
+    match typ with
+    | null -> box json
+    | typ -> json.ToObject(typ)
+    
+type EventAdapter(__ : Akka.Actor.ExtendedActorSystem) =
+
+    interface Akka.Persistence.Journal.IEventAdapter with
+
+        member __.Manifest(_ : obj) = 
+            let manifestType = typeof<Newtonsoft.Json.Linq.JObject>
+            sprintf "%s,%s" manifestType.FullName <| manifestType.Assembly.GetName().Name
+
+        member __.ToJournal(evt : obj) : obj = 
+            let jObject = Newtonsoft.Json.Linq.JObject.FromObject(evt)
+            jObject.AddFirst(Newtonsoft.Json.Linq.JProperty(eventType, evt.GetType().FullName));
+            Akka.Persistence.Journal.Tagged(box jObject, [| anyEventTag |]) :> obj
+
+        member __.FromJournal(evt : obj, _ : string) : Akka.Persistence.Journal.IEventSequence =
+            if evt :? Newtonsoft.Json.Linq.JObject then
+                Akka.Persistence.Journal.EventSequence.Single(deserializeEvent evt)
+            else
+                Akka.Persistence.Journal.EventSequence.Empty
+
+let adapterRef = typeof<EventAdapter>.FullName
+
+
+let quot = "\""
 let config = Configuration.parse """akka {  
     stdout-loglevel = WARNING
     loglevel = DEBUG
@@ -8,13 +44,11 @@ let config = Configuration.parse """akka {
         plugin = "akka.persistence.journal.sqlite"
         sqlite {
             class = "Akka.Persistence.Sqlite.Journal.SqliteJournal, Akka.Persistence.Sqlite"
-            plugin-dispatcher = "akka.actor.default-dispatcher"
             connection-string = "Data Source=JOURNAL.db;cache=shared;"
-            connection-timeout = 30s
-            schema-name = dbo
-            table-name = event_journal
             auto-initialize = on
-            timestamp-provider = "Akka.Persistence.Sql.Common.Journal.DefaultTimestampProvider, Akka.Persistence.Sql.Common"
+            event-adapters {
+              json-adapter = """ + quot + adapterRef + """, Upload"
+            }            
         }
     }
     actor {
@@ -27,6 +61,7 @@ let config = Configuration.parse """akka {
             unhandled = on
         }
     }
+
     }"""
 
 type ChatEvent =
@@ -40,31 +75,49 @@ type ChatMessage =
     | Command of ChatCommand
     | Event of ChatEvent
 
+
 [<EntryPoint>]
 let main argv =
     printfn "Persistance test"
 
-    let system = System.create "chatapp" (config.WithFallback <| Akkling.Configuration.defaultConfig())
+    let system = System.create "chatapp" config
 
-    let actor (ctx: Eventsourced<_>) =
+    let actor (mailbox: Eventsourced<_>) =
         let rec loop state = actor {
-            let! msg = ctx.Receive()
+            let! (msg: obj) = mailbox.Receive()
             match msg with
-            | Event(evt) when ctx.IsRecovering() ->
-                printfn "Got event while recovering"
-                return! loop (evt.Message :: state)
-            | Event(evt) ->
-                return! loop (evt.Message :: state)
-            | Command(cmd) ->
-                match cmd with
-                | GetMessages ->
-                    ctx.Sender() <! state
+            | :? ChatMessage as cm ->
+            match cm with
+                | Event evt when mailbox.IsRecovering() ->
+                    printfn "Got event while recovering"
+                    return! loop (evt.Message :: state)
+                | Event(evt) ->
+                    return! loop (evt.Message :: state)
+                | Command(cmd) ->
+                    match cmd with
+                    | GetMessages ->
+                        mailbox.Sender() <! state
+                        return! loop state
+                    | Message msg -> return Persist <| box(Event { Message = msg })
+
+            | :? LifecycleEvent as x -> printfn "Lifecycle %A" x; return! loop state
+            | :? PersistentLifecycleEvent as x -> printfn "Persistence Lifecycle %A" x; return! loop state          
+            | :? JObject as j when mailbox.IsRecovering() ->
+                match j.ToObject<ChatMessage>() with
+                | Event evt ->
+                    do printfn "%s" "Got JObject while recovering"
+                    return! loop (evt.Message :: state)
+                | x ->
+                    printfn "Unhandled JObject payload %A" x;
                     return! loop state
-                | Message msg -> return Persist (Event { Message = msg })
+            | x ->
+                printfn "Catch all %A" x;
+                return! loop state
         }
         loop []
 
-    let chat = spawn system "chat-1" <| propsPersist actor
+    let chat : ChatMessage IActorRef =
+        spawn system "chat-1" <| propsPersist actor |> retype
 
     chat <! Command (Message <| sprintf "New session started %A" System.DateTime.Now)
     async {
